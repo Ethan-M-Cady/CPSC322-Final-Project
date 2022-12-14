4f74649f637a3a7fe38ec2442bcff1c38311a850
Ethan-M-Cady <77717066+Ethan-M-Cady@users.noreply.github.com>
2022-12-13 21:24:19 -0800

    Update myclassifiers.py

--- mysklearn/myclassifiers.py
+++ mysklearn/myclassifiers.py
@@ -48,3 +48,66 @@ class MyNaiveBayesClassifier:
                     max_val = value
             y_predicted.append(max_str_value)
         return y_predicted
+    
+class MyDecisionTreeClassifier:
+    """Represents a decision tree classifier.
+    Attributes:
+        X_train(list of list of obj): The list of training instances (samples).
+                The shape of X_train is (n_train_samples, n_features)
+        y_train(list of obj): The target y values (parallel to X_train).
+            The shape of y_train is n_samples
+        tree(nested list): The extracted tree model.
+    Notes:
+        Loosely based on sklearn's DecisionTreeClassifier:
+            https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html
+        Terminology: instance = sample = row and attribute = feature = column
+    """
+
+    def __init__(self):
+        """Initializer for MyDecisionTreeClassifier.
+        """
+        self.X_train = None
+        self.y_train = None
+        self.tree = None
+
+    def fit(self, X_train, y_train):
+        """Fits a decision tree classifier to X_train and y_train using the TDIDT
+        (top down induction of decision tree) algorithm.
+        Args:
+            X_train(list of list of obj): The list of training instances (samples).
+                The shape of X_train is (n_train_samples, n_features)
+            y_train(list of obj): The target y values (parallel to X_train)
+                The shape of y_train is n_train_samples
+        Notes:
+            Since TDIDT is an eager learning algorithm, this method builds a decision tree model
+                from the training data.
+            Build a decision tree using the nested list representation described in class.
+            On a majority vote tie, choose first attribute value based on attribute domain ordering.
+            Store the tree in the tree attribute.
+            Use attribute indexes to construct default attribute names (e.g. "att0", "att1", ...).
+        """
+        self.X_train = X_train
+        self.y_train = y_train
+        train = [X_train[i] + [y_train[i]] for i in range(len(X_train))]
+        available_attributes = list(range(len(train[0])-1))
+        self.tree = myutils.tdidt(train, available_attributes, X_train)
+        # print(self.tree)
+
+    def predict(self, X_test):
+        """Makes predictions for test instances in X_test.
+        Args:
+            X_test(list of list of obj): The list of testing samples
+                The shape of X_test is (n_test_samples, n_features)
+        Returns:
+            y_predicted(list of obj): The predicted target y values (parallel to X_test)
+        """
+        header = []
+        for header_len in range(len(X_test[0])):
+            header.append("att" + str(header_len))
+
+        y_predicted = []
+        for instance in X_test:
+            predicted = myutils.tdidt_predict(header, self.tree, instance)
+            y_predicted.append(predicted)
+
+        return y_predicted
